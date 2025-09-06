fe6633426122cf82ef7e74cb1afcdd221737e08b
Dablestar <jbynchoi@gmail.com>
2025-09-06 21:08:44 +1000

    project configuration

---
+++ front/src/components/Footer.tsx
@@ -0,0 +1,14 @@
+function Footer(){
+    return (
+        <footer className="footer">
+            <p>© Copyright by Daburu 2025 All rights reserved.</p>
+            <div className="sns">
+                <a href="#"><img src="path/to/icon1.png" alt="Social Icon 1" /></a>
+                <a href="#"><img src="path/to/icon2.png" alt="Social Icon 2" /></a>
+                <a href="#"><img src="path/to/icon3.png" alt="Social Icon 3" /></a>
+            </div>
+        </footer>
+
+
+    )
+}
---
+++ front/src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+
+
+function NavBar(){
+    return(
+        <nav>
+            <a href="/">Home</a>
+            <a href="/aboutMe">About</a>
+            <a href="/projects">Projects</a>
+            <a href="/contact">Contact</a>
+        </nav>
+    )
+}
+export default NavBar
