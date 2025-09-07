ec2f2c9cb30c5307a72d2782578d2e7f36023e71
Dablestar <jbynchoi@gmail.com>
2025-09-07 19:29:23 +1000

    layout edit

--- docs/src/components/AboutMe.tsx
+++ docs/src/components/AboutMe.tsx
@@ -1,9 +1,15 @@
+import utsLogo from '../assets/uts.png'
+import utsCollegeLogo from '../assets/utscollege.png'
+import yydhLogo from '../assets/yydh.webp'
+import profile from '../assets/profile.jpg'
+import "../index.css"
+
 function AboutMe(){
     return(
          <div className="aboutMe m-4 p-4 w-full h-full">
             <span className="aboutMeText text-2xl font-bold">About Me</span>
 
-            <div className="personalInfo">
+            <div className="personalInfo flex flex-row gap-8 mt-4">
                 <div className="">
                     <p className="title text-xl font-bold"> Personal Information</p>
                     <ul>
@@ -13,13 +19,13 @@ function AboutMe(){
                     </ul>
                 </div>
                 <div className="profilePic">
-                    <img className="w-48 h-48 rounded-full" src="https://avatars.githubusercontent.com/u/55615385?v=4" alt="Profile Picture"/>
+                    <img className="w-24 h-32" src={profile} alt="Profile Picture"/>
                 </div>
             </div>
-            <div className="educationInfo">
+            <div className="educationInfo flex flex-col gap-4 mt-4">
                 <p className="title text-xl font-bold">Education</p>
-                <div className="school">
-                    <img src="" alt="" className="schoolLogo"/>
+                <div className="school flex flex-row">
+                    <img src={utsLogo} alt="" className="schoolLogo"/>
                     <ul className="schoolInfo">
                         <li>University of Technology Sydney</li>
                         <li>Bachelor of Information Technology</li>
@@ -31,8 +37,8 @@ function AboutMe(){
                         <li></li>
                     </ul>
                 </div>
-                <div className="school">
-                    <img src="" alt="" className="schoolLogo"/>
+                <div className="school flex flex-row">
+                    <img src={utsCollegeLogo} alt="" className="schoolLogo"/>
                     <ul className="schoolInfo">
                         <li>UTS College</li>
                         <li>Diploma of Information Technology</li>
@@ -44,8 +50,8 @@ function AboutMe(){
                         <li></li>
                     </ul>
                 </div>
-                <div className="school">
-                    <img src="" alt="" className="schoolLogo"/>
+                <div className="school flex flex-row">
+                    <img src={yydhLogo} alt="" className="schoolLogo"/>
                     <ul className="schoolInfo">
                         <li>Yang-Young Digital Highschool</li>
                         <li>Software Development Major</li>
