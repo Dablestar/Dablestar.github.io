a6b41d4af7d84209a93885713329253a5672a4c1
Dablestar <jbynchoi@gmail.com>
2025-09-09 23:45:19 +1000

    added scroll reset

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, ScrollRestoration} from 'react-router-dom'
 
 import Home from './components/Home'
 import AboutMe from './components/AboutMe'
@@ -25,6 +25,7 @@ function App() {
           <Footer />
         </div>
       </BrowserRouter>
+      <ScrollRestoration />
     </>
   )
 }
