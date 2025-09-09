9f7e52d49a5b5adf2cf81311044002f49514b6ab
Dablestar <jbynchoi@gmail.com>
2025-09-09 19:34:47 +1000

    added youtube srcs and fixed html file

--- docs/src/components/Hobbies.tsx
+++ docs/src/components/Hobbies.tsx
@@ -2,6 +2,56 @@ function Hobbies(){
     return(
         <div>
             <span className="hobbiesText text-2xl">Hobbies</span>
+            <div className="hobbiesContainer grid grid-col-1 grid-row-4">
+                <div className="hobby flex flex-col">
+                    <div className="hobbyTitle">
+                            Rhythm Game - Hand
+                    </div>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/FAlUwCnmMck?si=xuptKf3Vab2rdl6N&amp;start=73" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+                    <div className="hobbyDetails">
+
+                    </div>
+                </div>
+                <div className="hobby">
+                    <div className="imgContainer">
+                        <img src="" alt="" />
+                    </div>
+                    <div className="hobbyDetails">
+                        <div className="hobbyTitle">
+                            Rhythm Game - Hand
+                        </div>
+                        <div className="hobbyExplanation">
+
+                        </div>
+                    </div>
+                </div>
+                <div className="hobby">
+                    <div className="imgContainer">
+                        <img src="" alt="" />
+                    </div>
+                    <div className="hobbyDetails">
+                        <div className="hobbyTitle">
+                            Rhythm Game - Hand
+                        </div>
+                        <div className="hobbyExplanation">
+
+                        </div>
+                    </div>
+                </div>
+                <div className="hobby">
+                    <div className="imgContainer">
+                        <img src="" alt="" />
+                    </div>
+                    <div className="hobbyDetails">
+                        <div className="hobbyTitle">
+                            Rhythm Game - Hand
+                        </div>
+                        <div className="hobbyExplanation">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
