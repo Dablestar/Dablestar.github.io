1a1ca35590e293beca7c540268c2b96a5e88b88b
Dablestar <jbynchoi@gmail.com>
2025-09-09 14:13:31 +1000

    hobby layout

--- docs/src/components/Hobbies.tsx
+++ docs/src/components/Hobbies.tsx
@@ -2,6 +2,21 @@ function Hobbies(){
     return(
         <div>
             <span className="hobbiesText text-2xl">Hobbies</span>
+            <div className="hobbiesContainer grid grid-col-1 grid-row-4">
+                <div className="hobby">
+                    <div className="imgContainer">
+                        <img src="" alt="" />
+                    </div>
+                    <div className="hobbyDetails">
+                        <div className="hobbyTitle">
+                            RhythmGame
+                        </div>
+                        <div className="hobbyExplanation">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
