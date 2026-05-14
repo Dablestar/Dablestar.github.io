b8cf4bd187c056548968fbafcc279be521fb496a
Dablestar <jbynchoi@gmail.com>
2026-05-14 19:01:24 +1000

    added img resources for portfolio page

--- front/src/components/Portfolio.tsx
+++ front/src/components/Portfolio.tsx
@@ -1,23 +1,40 @@
-const images = {
-    project1: "",
-    project2: "",
-    project3: "",
-    project4: "",
-    project5: "",
-    project6: "",
-    project7: "",
-    project8: "",
+import jsLogo from "../assets/js.jpg"
+import tsLogo from "../assets/ts.png"
+import reactLogo from "../assets/react.png"
+import nodeLogo from "../assets/node.png"
+import mongoLogo from "../assets/mongo.png"
+import androidLogo from "../assets/android.png"
+import figmaLogo from "../assets/figma.png"
+import javaLogo from "../assets/java.png"
+import gcpLogo from "../assets/gcp.png"
+import blazorLogo from "../assets/blazor.png"
+import azureLogo from "../assets/azure.png"
+import type { JSX } from "react/jsx-dev-runtime"
+
+import project1Img from "../assets/projects/project1.png"
+import project2Img from "../assets/projects/project2.png"
+import project3Img from "../assets/projects/project3.png"
+import project4Img from "../assets/projects/project4.png"
+
+
+const images: { [key: string]: JSX.Element } = {
+    project1: <img src={project1Img} alt="Project 1" className="w-45 h-40 border object-fit"/>,
+    project2: <img src={project2Img} alt="Project 2" className="w-45 h-40 border object-fit"/>,
+    project3: <img src={project3Img} alt="Project 3" className="w-45 h-40 border object-fit"/>,
+    project4: <img src={project4Img} alt="Project 4" className="w-45 h-40 border object-fit "/>,
 }
 const logos: { [key: string]: JSX.Element } = {
-    react: <img src="" alt="" className="w-10 h-10" />,
-    nodejs: <img src="" alt="" className="w-10 h-10" />,
-    mongodb: <img src="" alt="" className="w-10 h-10" />,
-    android: <img src="" alt="" className="w-10 h-10" />,
-    figma: <img src="" alt="" className="w-10 h-10" />,
-    java: <img src="" alt="" className="w-10 h-10" />,
-    gcp: <img src="" alt="" className="w-10 h-10" />,
-    typescript: <img src="" alt="" className="w-10 h-10" />,
-    javascript: <img src="" alt="" className="w-10 h-10" />
+    react: <img src={reactLogo} alt="React" className="w-11 h-10" />,
+    nodejs: <img src={nodeLogo} alt="Node.js" className="w-10 h-10" />,
+    mongodb: <img src={mongoLogo} alt="MongoDB" className="w-10 h-10" />,
+    android: <img src={androidLogo} alt="Android" className="w-10 h-10" />,
+    figma: <img src={figmaLogo} alt="Figma" className="w-10 h-10" />,
+    java: <img src={javaLogo} alt="Java" className="w-10 h-10" />,
+    gcp: <img src={gcpLogo} alt="GCP" className="w-10 h-10" />,
+    typescript: <img src={tsLogo} alt="TypeScript" className="w-10 h-10" />,
+    javascript: <img src={jsLogo} alt="JavaScript" className="w-10 h-10" />,
+    blazor: <img src={blazorLogo} alt="Blazor" className="w-10 h-10" />,
+    azure: <img src={azureLogo} alt="Azure" className="w-10 h-10" />
 }
 
 
@@ -30,9 +47,13 @@ function Portfolio() {
             <span className="title text-2xl font-regular">Development Projects</span>
             <div className="dev grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 mt-5 mb-10">
                 <div className="project-card border p-4 rounded-lg shadow-md flex flex-row gap-4">
-                    <img src={images.project1} alt="Project 1" className="w-60 h-40 border"/>
+                    {images.project1}
                     <div>
-                        <h2 className="text-2xl font-regular">Project 1</h2>
+                        <div>
+                            <h2 className="text-2xl font-semibold">Washare</h2>
+                            <h4 className="text-sm font-regular">desc1</h4>
+                        </div>
+                        
                         <div className="flex flex-row mt-2">
                             <span className="font-regular">Tech Stack:</span>
                             {/* put stack logos here */}
@@ -47,9 +68,12 @@ function Portfolio() {
                     
                 </div>
                 <div className="project-card border p-4 rounded-lg shadow-md flex flex-row gap-4">
-                    <img src={images.project1} alt="Project 1" className="w-60 h-40 border"/>
+                    {images.project2}
                     <div>
-                        <h2 className="text-2xl font-regular">Project 1</h2>
+                        <div>
+                            <h2 className="text-2xl font-semibold">Project 2</h2>
+                            <h4 className="text-sm font-regular">desc2</h4>
+                        </div>
                         <div className="flex flex-row mt-2">
                             <span className="font-regular">Tech Stack:</span>
                             {/* put stack logos here */}
@@ -64,9 +88,12 @@ function Portfolio() {
                     
                 </div>
                 <div className="project-card border p-4 rounded-lg shadow-md flex flex-row gap-4">
-                    <img src={images.project1} alt="Project 1" className="w-60 h-40 border"/>
+                    {images.project3}
                     <div>
-                        <h2 className="text-2xl font-regular">Project 1</h2>
+                        <div>
+                            <h2 className="text-2xl font-semibold">Songblocker</h2>
+                            <h4 className="text-sm font-regular">desc3</h4>
+                        </div>
                         <div className="flex flex-row mt-2">
                             <span className="font-regular">Tech Stack:</span>
                             {/* put stack logos here */}
@@ -76,14 +103,14 @@ function Portfolio() {
                                 <li>{logos.mongodb}</li>
                             </ul>
                         </div>
-                        <p className="text-lg font-thin">Description of Project 1. This project involved...</p>    
+                        <p className="text-lg font-thin">Description of Songblocker. This project involved...</p>    
                     </div>
                     
                 </div>
                 <div className="project-card border p-4 rounded-lg shadow-md flex flex-row gap-4">
-                    <img src={images.project1} alt="Project 1" className="w-60 h-40 border"/>
+                    {images.project4}
                     <div>
-                        <h2 className="text-2xl font-regular">Project 1</h2>
+                        <h2 className="text-2xl font-regular">Project 4</h2>
                         <div className="flex flex-row mt-2">
                             <span className="font-regular">Tech Stack:</span>
                             {/* put stack logos here */}
@@ -93,7 +120,7 @@ function Portfolio() {
                                 <li>{logos.mongodb}</li>
                             </ul>
                         </div>
-                        <p className="text-lg font-thin">Description of Project 1. This project involved...</p>    
+                        <p className="text-lg font-thin">Description of Project 4. This project involved...</p>    
                     </div>
                     
                 </div>
